c7c11d3ef8c65ffb4a34922d95c58027393b9bab
Frozan Shadab <frozan.shadab12@gmail.com>
2025-04-21 11:33:41 +0430

    add some changes

--- src/component/Project2.jsx
+++
@@ -1,4 +0,0 @@
-export default function project2(){
-
-    
-}
--- src/pages/project2.js
+++ src/pages/project2.js
@@ -7,11 +7,11 @@ export default function Project2() {
         Project (2)
       </h1>
 
-      <div className="bg-gray-500 mx-auto rounded-lg p-6 w-1/2">
+      <div className="bg-gray-100 mx-auto rounded-lg p-6 w-1/2">
         <h2 className="text-2xl font-bold text-gray-800 mb-6"> FAQs</h2>
 
         <div className="mb-4">
-          <h3 className="w-full text-left bg-gray-200 shadow-cyan-100 rounded-lg h-8 font-semibold text-gray-800 flex justify-between items-center">
+          <h3 className="w-full text-left bg-gray-200 shadow-gray-500 rounded-lg h-8 font-semibold text-gray-800 flex justify-between items-center">
             What is coding?
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -30,7 +30,7 @@ export default function Project2() {
           </h3>
         </div>
 
-        <div className="mb-4 bg-cyan-300 p-4 rounded">
+        <div className="mb-4 bg-[#32aaaf] p-4 rounded">
           <h3 className="font-semibold text-gray-800 flex justify-between items-center mb-2">
             Which programming language should I learn first?
             <svg
@@ -56,7 +56,7 @@ export default function Project2() {
         </div>
 
         <div className="mb-4">
-          <h3 className="w-full text-left font-semibold bg-gray-200 shadow-cyan-100 rounded-lg h-8 text-gray-800 flex justify-between items-center">
+          <h3 className="w-full text-left font-semibold bg-gray-200 shadow-gray-500 rounded-lg h-8 text-gray-800 flex justify-between items-center">
             How do I improve my coding skills? 
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -76,7 +76,7 @@ export default function Project2() {
         </div>
 
         <div className="mb-4">
-          <h3 className="w-full text-left font-semibold bg-gray-200 shadow-cyan-100 rounded-lg h-8 text-gray-800 flex justify-between items-center">
+          <h3 className="w-full text-left font-semibold bg-gray-200 shadow-gray-500 rounded-lg h-8 text-gray-800 flex justify-between items-center">
             How do I debug my code effectively?
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -96,7 +96,7 @@ export default function Project2() {
         </div>
 
         <div>
-          <h3 className="w-full text-left font-semibold bg-gray-200 shadow-cyan-100 rounded-lg h-8 text-gray-800 flex justify-between items-center">
+          <h3 className="w-full text-left font-semibold bg-gray-200 shadow-gray-500 rounded-lg h-8 text-gray-800 flex justify-between items-center">
             What are APIs, and how do they work?
             <svg
               xmlns="http://www.w3.org/2000/svg"
